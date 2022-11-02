db39111cddb76e62df88d97c2c54b43464d5aff4
mhemmer-cern <mhemmer-cern@users.noreply.github.com>
2022-11-02 16:35:42 +0000

    deploy: aea80f36aeda6c59aca39482a3015fc149aee582

--- navtreedata.js
+++ navtreedata.js
@@ -29,43 +29,18 @@ var NAVTREE =
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
         [ "All", "namespacemembers.html", null ],
-        [ "Functions", "namespacemembers_func.html", null ],
-        [ "Variables", "namespacemembers_vars.html", null ],
-        [ "Typedefs", "namespacemembers_type.html", null ],
-        [ "Enumerations", "namespacemembers_enum.html", null ],
-        [ "Enumerator", "namespacemembers_eval.html", null ]
-      ] ]
-    ] ],
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", "functions_dup" ],
-        [ "Functions", "functions_func.html", "functions_func" ],
-        [ "Variables", "functions_vars.html", null ],
-        [ "Related Functions", "functions_rela.html", null ]
+        [ "Functions", "namespacemembers_func.html", null ]
       ] ]
     ] ],
     [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ],
-      [ "File Members", "globals.html", [
-        [ "All", "globals.html", null ],
-        [ "Macros", "globals_defs.html", null ]
-      ] ]
+      [ "File List", "files.html", "files_dup" ]
     ] ]
   ] ]
 ];
 
 var NAVTREEINDEX =
 [
-"Helpers_8cxx.html",
-"classPlottingFramework_1_1PlotManager.html#a5f504cde0cb9c657e138ce02d8fbc0b3",
-"classPlottingFramework_1_1Plot_1_1Pad.html#a9be64288e381e87d0ad0efe8fe82b83c",
-"classPlottingFramework_1_1Plot_1_1Pad_1_1Box.html#a1e8b3483350719976f0842d66e2c583c",
-"classPlottingFramework_1_1Plot_1_1Pad_1_1Data.html#ac00a2eb819245ea9c00eadbc6ffdfa1a",
-"classPlottingFramework_1_1Plot_1_1Pad_1_1Ratio.html#a2a0fbd6d33719a06a4fe8efa3e70f92f",
-"namespacePlottingFramework.html#aedca3c6b537e072eb14a5d133813aa7ca8940c83cf33e4324df76a4b5d36dec23"
+"Helpers_8cxx.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
